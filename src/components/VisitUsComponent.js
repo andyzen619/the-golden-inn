e8fe7fd75529260d3b899aae91bcf4123cf47c4b
Andy Liang <andyzen619@gmail.com>
2020-01-24 18:02:14 -0500

    Add hours of operation

--- src/components/VisitUsComponent.js
+++ src/components/VisitUsComponent.js
@@ -1,13 +1,33 @@
 import React from "react";
 
-import {} from "semantic-ui-react";
+import {Header, List} from "semantic-ui-react";
+import "./VisitUsComponent.css";
 
 const VisitUsComponent = () => {
 
-  
+  const daysOfTheWeek = [
+    'Mon. Closed', 
+    'Tue. 11:30 a.m. – 09:00 p.m.', 
+    'Wed. 11:30 a.m. – 09:00 p.m.',
+    'Thurs. 11:30 a.m. – 09:00 p.m.',
+    'Fri. 11:30 a.m. – 09:00 p.m.',
+    'Sat. 11:30 a.m. – 09:00 p.m.',
+    'Sun. 11:30 a.m. – 09:00 p.m.'
+  ]
 
   return (
-    <div>
+    <div className='visitUsDivStyle'>
+      <Header size="large" style={{color: 'black'}}>VISIT US</Header>
+      <Header size="tiny" style={{color: 'grey'}}>155 Main Street West, Picton, Prince Edward, ON, Canada</Header>
+      <Header size = 'medium'>Hours of operation</Header>
+      <List>
+        {daysOfTheWeek.map((day) => {
+          console.log(day);
+        return <List.Item>{day}</List.Item>
+        })}
+      </List>
     </div>
   );
 }
+
+export default VisitUsComponent;
