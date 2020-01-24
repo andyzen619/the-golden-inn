39fa686e42a9cf1cf3443ffe155a547a250e8726
Andy Liang <andyzen619@gmail.com>
2020-01-24 17:03:30 -0500

    Add component skeleton

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.css";
+
 import LandingpageComponent from "./components/LandingComponent";
+import VisitUsComponenet from "./components/VisitUsComponent";
 
 import {} from "semantic-ui-react";
 
@@ -8,6 +10,7 @@ function App() {
   return (
     <div>
       <LandingpageComponent/>
+      <VisitUsComponenet/>
     </div>
   );
 }
---
+++ src/components/VisitUsComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import {} from "semantic-ui-react";
+
+const VisitUsComponent = () => {
+
+  
+
+  return (
+    <div>
+    </div>
+  );
+}
