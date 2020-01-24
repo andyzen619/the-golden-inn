1b9d4cb94ea0ea6f62a01fc063122458ebc5c3c0
Andy Liang <andyzen619@gmail.com>
2020-01-24 16:55:26 -0500

    Complete landing page image, centered message, adjusted brighness

--- src/components/LandingComponent.js
+++ src/components/LandingComponent.js
@@ -1,15 +1,20 @@
 import React from "react";
+import "./LandingComponent.css";
 
-import {} from "semantic-ui-react";
+import { Image, Header } from "semantic-ui-react";
 
 const LandingComponent = () => {
-
   return (
     <div>
-      The Golden Inn
-    </div> 
+      <Image src="https://img1.wsimg.com/isteam/stock/107160/:/rs=h:1028,m" className="image"/>
+      <div className="divStyle">
+        <Header size="huge" style={{color: 'white'}}>WELCOME TO THE GOLDEN INN</Header>
+        <Header size="small" style={{color: 'white'}}>
+          Serving authentic Chinese food for over 25 years
+        </Header>
+      </div>
+    </div>
   );
-
 };
 
 export default LandingComponent;
