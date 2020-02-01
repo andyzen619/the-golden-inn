2e7245f930b8b5c10553dc04bbaf9190eef92b08
Andy Liang <andyzen619@gmail.com>
2020-02-01 17:18:06 -0500

    add: responsive navbar

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 
 import LandingpageComponent from "./components/LandingComponent";
 import VisitUsComponenet from "./components/VisitUsComponent";
+import NavBarComponent from "./components/NavbarComponent";
 
 import {} from "semantic-ui-react";
 
 function App() {
   return (
     <div style={{background: '#d9c8b5'}}>
+      <NavBarComponent/>
       <LandingpageComponent/>
       <VisitUsComponenet/>
     </div>
