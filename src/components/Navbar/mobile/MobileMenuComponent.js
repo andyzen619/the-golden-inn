854680e6db817a2ea66d4d1e92b5283b584bb125
Andy Liang <andyzen619@gmail.com>
2020-02-13 02:07:45 -0500

    add: menu modal dimmer to blurring fix: decrease size of menu

--- src/components/Navbar/mobile/MobileMenuComponent.js
+++ src/components/Navbar/mobile/MobileMenuComponent.js
@@ -3,23 +3,16 @@ import { Button, Header, Icon, Modal, Menu } from 'semantic-ui-react'
 import MenuListComponenet from '../../menu/MenuListComponent';
 
 const MenuStyle = {
-  height: "inherit"
+  top: '5%',
+  width: '90%'
 }
 
 const MenuComponent = () => (
-  <Modal trigger={<Menu.Item position="left">Menu</Menu.Item>} closeIcon style={MenuStyle}>
-    <Modal.Content>
+  <Modal trigger={<Menu.Item position="left">Menu</Menu.Item>} closeIcon style={MenuStyle} dimmer='blurring'>
+    <Modal.Content scrolling>
       <Header content='Menu' />
       <MenuListComponenet/>
     </Modal.Content>
-    <Modal.Actions>
-      {/* <Button color='red'>
-        <Icon name='remove' /> No
-      </Button>
-      <Button color='green'>
-        <Icon name='checkmark' /> Yes
-      </Button> */}
-    </Modal.Actions>
   </Modal>
 )
 
