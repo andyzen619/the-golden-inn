144cf4da11a94d269393cdf67120547b86305a4a
Andy Liang <andyzen619@gmail.com>
2020-02-13 02:17:05 -0500

    fix: menu modal height

--- src/components/Navbar/mobile/MobileMenuComponent.js
+++ src/components/Navbar/mobile/MobileMenuComponent.js
@@ -4,12 +4,13 @@ import MenuListComponenet from '../../menu/MenuListComponent';
 
 const MenuStyle = {
   top: '5%',
-  width: '90%'
+  width: '90%',
+  height: '80%'
 }
 
 const MenuComponent = () => (
   <Modal trigger={<Menu.Item position="left">Menu</Menu.Item>} closeIcon style={MenuStyle} dimmer='blurring'>
-    <Modal.Content scrolling>
+    <Modal.Content style={{maxHeight: 'calc(100vh - 10em)', overflow: 'auto'}}>
       <Header content='Menu' />
       <MenuListComponenet/>
     </Modal.Content>
