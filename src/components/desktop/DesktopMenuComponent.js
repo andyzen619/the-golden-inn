b7583ee20d1ac9433117e7b2f68fd807ba627347
Andy Liang <andyzen619@gmail.com>
2020-02-18 15:53:16 -0500

    refactor: sidbar component

--- src/components/desktop/DesktopMenuComponent.js
+++ src/components/desktop/DesktopMenuComponent.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
 import {
   Accordion,
   Sidebar,
@@ -7,6 +8,7 @@ import {
   Image,
   Header
 } from "semantic-ui-react";
+import SideBarComponent from '../desktop/menu/SidebarComponent';
 
 const DesktopMenuComponentStyle = {
   sidebarContainerStyle: { height: "600px", background: "white" },
@@ -14,66 +16,11 @@ const DesktopMenuComponentStyle = {
 };
 
 const DesktopMenuComponent = () => {
-  const { sidebarContainerStyle, sidebarStyle } = DesktopMenuComponentStyle;
-  const [activeIndex, setActiveIndeex] = useState(0);
-
-  const handleClick = (e, titleProps) => {
-    const { index } = titleProps;
-    const newIndex = activeIndex === index ? -1 : index;
-
-    setActiveIndeex(newIndex);
-  };
-
+  const { sidebarContainerStyle} = DesktopMenuComponentStyle;
+  
   return (
     <Sidebar.Pushable style={sidebarContainerStyle}>
-      <Sidebar
-        as={Accordion}
-        animation="push"
-        icon="labeled"
-        vertical
-        visible={true}
-        width="wide"
-        styled
-      >
-        <Accordion.Title>
-          <Icon />
-          Special Combination Plates
-        </Accordion.Title>
-
-        <Accordion.Title>
-          <Icon />
-          Full Course Dinners
-        </Accordion.Title>
-
-        <Accordion.Title
-          active={activeIndex === 1}
-          index={1}
-          onClick={handleClick}
-        >
-          <Icon name="dropdown" />
-          Main Dishes
-        </Accordion.Title>
-        <Accordion.Content active={activeIndex === 1} styled={false}>
-          <Accordion style={{boxShadow: 'none'}}>
-            <Accordion.Title>
-              Soups
-            </Accordion.Title>
-            <Accordion.Title>
-              Appetizers
-            </Accordion.Title>
-            <Accordion.Title>
-              Rice (One size)
-            </Accordion.Title>
-            <Accordion.Title>
-              Egg Foo Young (Grilled)
-            </Accordion.Title>
-            <Accordion.Title>
-              Chow Mein/Chop Suey
-            </Accordion.Title>
-          </Accordion>
-        </Accordion.Content>
-      </Sidebar>
-
+      <SideBarComponent/>
       <Sidebar.Pusher>
         <Segment basic>
           <Header as="h3">Application Content</Header>
--- src/components/desktop/menu/SidebarComponent.js
+++ src/components/desktop/menu/SidebarComponent.js
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
-import { Sidebar, Accordion, Icon, Menu } from "semantic-ui-react";
+import { Sidebar, Accordion, Icon} from "semantic-ui-react";
 import { Link, BrowserRouter as Router, Route } from "react-router-dom";
 
 const SidebarComponentStyle = {
-  sidebarContainerStyle: { background: "white" },
-  sidebarStyle: { padding: " 1.4em", position: "relative" }
+  sidebarStyle: { padding: " 1.4em" }
 };
 
 const SidebarComponent = () => {
@@ -17,57 +16,44 @@ const SidebarComponent = () => {
     setActiveIndeex(newIndex);
   };
 
-  const { sidebarContainerStyle, sidebarStyle } = SidebarComponentStyle;
   return (
-    <Accordion vertical>
-      <Accordion.Title>Special Combination Plates</Accordion.Title>
-      <Accordion.Title>Full Course Dinners</Accordion.Title>
-      <Accordion.Title active={activeIndex === 2}
-          index={2}
-          onClick={handleClick}>
-        <Accordion.Title>Main Dishes</Accordion.Title>
-        <Accordion.Content active={activeIndex === 2}>
-          <Menu animation="push" vertical="true" visible={true}>
-            <Accordion.Title>
-              <Icon />
-              Soup
-            </Accordion.Title>
-            <Accordion.Title>
-              <Icon />
-              Appetizers
-            </Accordion.Title>
-            <Accordion.Title>
-              <Icon />
-              Rice
-            </Accordion.Title>
-            <Accordion.Title>
-              <Icon />
-              Sweet and Sour
-            </Accordion.Title>
-            <Accordion.Title>
-              <Icon />
-              Egg Foo Young (Grilled)
-            </Accordion.Title>
-            <Accordion.Title>
-              <Icon />
-              Vegetables and Almonds
-            </Accordion.Title>
-            <Accordion.Title>
-              <Icon />
-              Chow Mein (dry noodles)/Chop Suey
-            </Accordion.Title>
-            <Accordion.Title>
-              <Icon />
-              Cantonese Style
-            </Accordion.Title>
-            <Accordion.Title>
-              <Icon />
-              Canadian Dishes
-            </Accordion.Title>
-          </Menu>
-        </Accordion.Content>
+    <Sidebar
+      as={Accordion}
+      animation="push"
+      icon="labeled"
+      vertical
+      visible={true}
+      width="wide"
+      styled
+    >
+      <Accordion.Title>
+        <Icon />
+        Special Combination Plates
       </Accordion.Title>
-    </Accordion>
+
+      <Accordion.Title>
+        <Icon />
+        Full Course Dinners
+      </Accordion.Title>
+
+      <Accordion.Title
+        active={activeIndex === 1}
+        index={1}
+        onClick={handleClick}
+      >
+        <Icon name="dropdown" />
+        Main Dishes
+      </Accordion.Title>
+      <Accordion.Content active={activeIndex === 1} styled={false}>
+        <Accordion style={{ boxShadow: "none" }}>
+          <Accordion.Title>Soups</Accordion.Title>
+          <Accordion.Title>Appetizers</Accordion.Title>
+          <Accordion.Title>Rice (One size)</Accordion.Title>
+          <Accordion.Title>Egg Foo Young (Grilled)</Accordion.Title>
+          <Accordion.Title>Chow Mein/Chop Suey</Accordion.Title>
+        </Accordion>
+      </Accordion.Content>
+    </Sidebar>
   );
 };
 
