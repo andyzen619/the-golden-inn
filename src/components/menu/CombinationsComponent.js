d35b06eb2341e0cba0ccd83be81e086ba8a2018b
Andy Liang <andyzen619@gmail.com>
2020-02-22 18:31:47 -0500

    add: image to cantonese and combination plates

--- src/components/menu/CombinationsComponent.js
+++ src/components/menu/CombinationsComponent.js
@@ -5,7 +5,8 @@ import {
   Header,
   Container,
   List,
-  Item
+  Item,
+  Image
 } from "semantic-ui-react";
 import { MenuContext } from "../context/menu-context";
 import Media from "react-media";
@@ -30,21 +31,27 @@ const CombinationsComponent = () => {
             <List>
               {getCombinations().map(dish => {
                 return (
-                  <List.Item key={dish.name}>
+                  <List.Item key={dish.name} style={{paddingTop: '5%'}}>
                     <Card.Content>
                       <Header
                         as="h4"
                         floated="right"
                         color="green"
-                        style={{ fontWeight: "400" }}
+                        style={{ fontWeight: "400", position: 'relative', bottom:'25px' }}
                       >
                         {dish.price}
                       </Header>
-                      <Card.Header>
+                      <Card.Header style={{position: 'relative', right: '15px', bottom: '25px'}}>
+                        <Image
+                          avatar={true}
+                          src="https://lh3.googleusercontent.com/RE7beBVICIcwh8t3jKUWicPiJOkzFu81-L1eXdVnBkpaN7uRdU5y-_MhqLpuA0HN5Ss9uWeentAAohxBWCWy8oV3r5846ePHSfU_ZMlN8snPkcgjgU5RQVxIkPixImpays84AoXSOWIt-quHJPAHYPd6OG8Qrq7jCGU7BWoVGzhnHf0JdOnlPPY1-RGPcu420aU_29RqW9z1qm_TLYyffj-gPWQyytt8PaRd5NZPGVSLEd3YD9Q9bJUYiopMpCPJr0xQdn96OHYTCsd7YsWYf2hI_fj4ptn54Bux9EQO4qRRLjXI1hBkCtbv-9qWp0QwX_-51UX9jRo5Q1vCtOMyGZSm7FRUxIlnWkLMTjbGO2AGtcOMXrXTJYbSTCZ1h1Nv-NYblvM6lWEp-XwAzP8DwIxTIcTf50JdWY20I0dZ_fOgd1AhuXNi4APB8OhSkS4DKpi1fiRGrDdac57CtwgAiP5yKentsTEU0CtL7SSkBlBwh5pAcRrfDpFTf0csVa2kd90AvfWiJW-RuOtHHZF214aM76g9R74Kq-UTi4jHTSxJJ39Oc7vbomOtGnPW_bxrX9AHMFeDQGUH9lU22RkzbOI4bXtqxpLX03cslRjLOrWVknPj8an3gZMUfM_L5oiXUbYLBYoduLl5wYx1rImwrKZ8HXrcSpWXRC3Ao7cqJ290wrhoI3rh0A=w1195-h865-no"
+                          size="tiny"
+                          floated="left"
+                        />
                         <Header as="h4">{dish.name}</Header>
                       </Card.Header>
                       {dish.description && (
-                        <Card.Description>
+                        <Card.Description style={{display: 'flex', position: 'relative', bottom: '25px'}}>
                           <Header as="h5" color="grey">
                             {dish.description}
                           </Header>
