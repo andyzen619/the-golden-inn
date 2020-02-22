095a5b74e5b2315ced7cdd84912868d2c9274559
Andy Liang <andyzen619@gmail.com>
2020-02-22 16:12:30 -0500

    add: kung po chicken picture

--- src/components/menu/dishes/CantoneseComponent.js
+++ src/components/menu/dishes/CantoneseComponent.js
@@ -1,5 +1,12 @@
 import React, { useContext, Fragment } from "react";
-import { Card, Header, List } from "semantic-ui-react";
+import {
+  Card,
+  Header,
+  List,
+  Image,
+  Segment,
+  SegmentGroup
+} from "semantic-ui-react";
 import { MenuContext } from "../../context/menu-context";
 import Media from "react-media";
 const CantoneseComponentComponent = () => {
@@ -19,18 +26,23 @@ const CantoneseComponentComponent = () => {
       {matches => (
         <Fragment>
           {matches.mobile && (
-            <List>
+            <Segment.Group>
               {getCantoneseComponent().map(dish => {
                 return (
-                  <List.Item key={dish.name}>
+                  <Segment key={dish.name}>
                     <Card.Content>
                       <Header
                         as="h4"
                         floated="right"
                         color="green"
-                        style={{ fontWeight: "400" }}
+                        style={{ fontWeight: "400", position: 'relative', left: '30px' }}
                       >
                         {dish.price}
+                        <Image
+                          src="https://lh3.googleusercontent.com/RE7beBVICIcwh8t3jKUWicPiJOkzFu81-L1eXdVnBkpaN7uRdU5y-_MhqLpuA0HN5Ss9uWeentAAohxBWCWy8oV3r5846ePHSfU_ZMlN8snPkcgjgU5RQVxIkPixImpays84AoXSOWIt-quHJPAHYPd6OG8Qrq7jCGU7BWoVGzhnHf0JdOnlPPY1-RGPcu420aU_29RqW9z1qm_TLYyffj-gPWQyytt8PaRd5NZPGVSLEd3YD9Q9bJUYiopMpCPJr0xQdn96OHYTCsd7YsWYf2hI_fj4ptn54Bux9EQO4qRRLjXI1hBkCtbv-9qWp0QwX_-51UX9jRo5Q1vCtOMyGZSm7FRUxIlnWkLMTjbGO2AGtcOMXrXTJYbSTCZ1h1Nv-NYblvM6lWEp-XwAzP8DwIxTIcTf50JdWY20I0dZ_fOgd1AhuXNi4APB8OhSkS4DKpi1fiRGrDdac57CtwgAiP5yKentsTEU0CtL7SSkBlBwh5pAcRrfDpFTf0csVa2kd90AvfWiJW-RuOtHHZF214aM76g9R74Kq-UTi4jHTSxJJ39Oc7vbomOtGnPW_bxrX9AHMFeDQGUH9lU22RkzbOI4bXtqxpLX03cslRjLOrWVknPj8an3gZMUfM_L5oiXUbYLBYoduLl5wYx1rImwrKZ8HXrcSpWXRC3Ao7cqJ290wrhoI3rh0A=w1195-h865-no"
+                          size="large"
+                          floated="right"
+                        />
                       </Header>
                       <Card.Header>
                         <Header as="h4">{dish.name}</Header>
@@ -43,10 +55,10 @@ const CantoneseComponentComponent = () => {
                         </Card.Description>
                       )}
                     </Card.Content>
-                  </List.Item>
+                  </Segment>
                 );
               })}
-            </List>
+            </Segment.Group>
           )}
           {!matches.mobile && (
             <Card.Group style={{ paddingBottom: "50px" }}>
