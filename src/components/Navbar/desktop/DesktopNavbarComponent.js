3c9555a9a8a8548a38a86c464cf52e3979d662d4
Andy Liang <andyzen619@gmail.com>
2020-02-24 16:54:56 -0500

    add: responsive logo size. Small logo when menu is selected, big logo when home page is selected

--- src/components/Navbar/desktop/DesktopNavbarComponent.js
+++ src/components/Navbar/desktop/DesktopNavbarComponent.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Responsive, Menu, Header, Image } from "semantic-ui-react";
 import MenuSearchComponent from "../MenuSearchComponent";
 
@@ -12,7 +12,46 @@ const DesktopNavbarComponentStyle = {
 };
 
 const DesktopNavbarComponent = () => {
+  const [bigLogo, setBigLogo] = useState(true);
   const { divStyle } = DesktopNavbarComponentStyle;
+
+  /**
+   * Updates to big logo state
+   */
+  const updateBigLogo = (flag) => {
+    setBigLogo(true);
+  }
+
+  /**
+   * Updates to small logo state
+   */
+  const updateSmallLogo = () => {
+    setBigLogo(false);
+  }
+
+  /**
+   *Renders logo according to state
+   */
+  const generateLogo = () => {
+    if (bigLogo) {
+      return (
+        <Menu.Item style={{ position: "absolute", top: "5%", zIndex: "10" }} onClick={updateBigLogo}>
+          <Link to="/">
+            <Image size="small" src="https://i.imgur.com/7SBSyFL.png" />
+          </Link>
+        </Menu.Item>
+      );
+    } else {
+      return (
+        <Menu.Item onClick={updateBigLogo}>
+          <Link to="/">
+            <Image size="tiny" src="https://i.imgur.com/wTWZiYT.png" />
+          </Link>
+        </Menu.Item>
+      );
+    }
+  };
+
   return (
     <Responsive
       as={Menu}
@@ -21,19 +60,10 @@ const DesktopNavbarComponent = () => {
       fluid
       secondary
     >
-      <Menu.Item style={{position: 'absolute', top: "5%", zIndex: "10"}}>
-        <Link to="/">
-          <Image size="small" src="https://i.imgur.com/7SBSyFL.png" />
-        </Link>
-      </Menu.Item>
-      {/* <Menu.Item>
-        <Link to="/">
-          <Image size="tiny" src="https://i.imgur.com/wTWZiYT.png" />
-        </Link>
-      </Menu.Item> */}
+      {generateLogo()}
 
       <Menu.Menu position="right">
-        <Menu.Item name="Menu">
+        <Menu.Item name="Menu" onClick={updateSmallLogo}>
           <Link to="/menu">
             <Header as="h4">Menu</Header>
           </Link>
