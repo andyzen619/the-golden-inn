2384cdb2e7ff2d3ff6a7b8713255819506021ef6
Andy Liang <andyzen619@gmail.com>
2020-03-04 18:43:10 -0500

    add: flexbox to cantonese componenet

--- src/components/menu/dishes/CantoneseComponent.js
+++ src/components/menu/dishes/CantoneseComponent.js
@@ -13,6 +13,11 @@ import Media from "react-media";
 const CantoneseComponentComponent = () => {
   const [menu, setMenu] = useContext(MenuContext);
 
+  const menuStyle = {
+    display: 'flex',
+    justifyContent: 'space-evenly'
+  }
+
   const getCantoneseComponent = () => {
     if (menu.dishes) {
       let CantoneseComponent = menu.dishes.cantonese;
@@ -81,45 +86,48 @@ const CantoneseComponentComponent = () => {
             </Segment.Group>
           )}
           {!matches.mobile && (
-            <Item.Group
-              style={{
-                background: "white",
-                padding: "5%",
-                boxShadow: "5px 5px 5px 5px black"
-              }}
-              divided
-            >
-              {getCantoneseComponent().map(dish => {
-                return (
-                  <Item key={dish.name}>
-                    <Card.Content>
-                      <Header
-                        as="h3"
-                        floated="right"
-                        color="green"
-                        style={{ fontWeight: "400" }}
-                      >
-                        {dish.price}
-                      </Header>
-                      <Item.Header>
-                        <Header as="h4">{dish.name}</Header>
-                      </Item.Header>
-                      {dish.description && (
-                        <Item.Meta>{dish.description}</Item.Meta>
-                      )}
-                    </Card.Content>
-                    {dish.image && (
+            <div style={menuStyle}>
+              <Item.Group
+                style={{
+                  background: "white",
+                  padding: "5%",
+                  boxShadow: "5px 5px 5px 5px black",
+                  width: '60%'
+                }}
+                divided
+              >
+                {getCantoneseComponent().map(dish => {
+                  return (
+                    <Item key={dish.name}>
+                      <Card.Content>
+                        <Header
+                          as="h3"
+                          floated="right"
+                          color="green"
+                          style={{ fontWeight: "400" }}
+                        >
+                          {dish.price}
+                        </Header>
+                        <Item.Header>
+                          <Header as="h4">{dish.name}</Header>
+                        </Item.Header>
+                        {dish.description && (
+                          <Item.Meta>{dish.description}</Item.Meta>
+                        )}
+                      </Card.Content>
+                      {/* {dish.image && (
                       <Image
                         src={dish.image}
                         size="small"
                         rounded="true"
                         style={{ maxHeight: "150px", paddingLeft: '10px' }}
                       />
-                    )}
-                  </Item>
-                );
-              })}
-            </Item.Group>
+                    )} */}
+                    </Item>
+                  );
+                })}
+              </Item.Group>
+            </div>
           )}
         </Fragment>
       )}
