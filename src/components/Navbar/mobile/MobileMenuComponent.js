145a20dcae5bd6b1dcee6dda734e67f2a0599fd5
Andy Liang <andyzen619@gmail.com>
2020-03-11 01:14:46 -0400

    add: mobile, prices subject to change warning

--- src/components/Navbar/mobile/MobileMenuComponent.js
+++ src/components/Navbar/mobile/MobileMenuComponent.js
@@ -1,20 +1,33 @@
-import React from 'react'
-import { Button, Header, Icon, Modal, Menu } from 'semantic-ui-react'
-import MenuListComponenet from '../../menu/MenuListComponent';
+import React from "react";
+import { Button, Header, Icon, Modal, Menu } from "semantic-ui-react";
+import MenuListComponenet from "../../menu/MenuListComponent";
 
 const MenuStyle = {
-  top: '5%',
-  width: '90%',
-  height: '80%'
-}
+  top: "5%",
+  width: "90%",
+  height: "80%"
+};
 
 const MenuComponent = () => (
-  <Modal trigger={<Menu.Item position="left"><Header>Menu</Header></Menu.Item>} closeIcon style={MenuStyle} >
-    <Modal.Content style={{maxHeight: 'calc(100vh - 10em)', overflow: 'auto'}}>
-      <Header as='h2' content='Menu' />
-      <MenuListComponenet/>
+  <Modal
+    trigger={
+      <Menu.Item position="left">
+        <Header>Menu</Header>
+      </Menu.Item>
+    }
+    closeIcon
+    style={MenuStyle}
+  >
+    <Modal.Content
+      style={{ maxHeight: "calc(100vh - 10em)", overflow: "auto" }}
+    >
+      <Header as="h2">
+        Menu
+      </Header>
+      <MenuListComponenet />
+      <Header sub as='h6'>Prices are subject to change.**</Header>
     </Modal.Content>
   </Modal>
-)
+);
 
-export default MenuComponent
+export default MenuComponent;
