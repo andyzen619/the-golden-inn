2968f362366280d8bcb0aba78504dac844f82c8a
Andy Liang <andyzen619@gmail.com>
2020-03-20 10:22:09 -0400

    add: covid-19 message for mobile and desktop

--- src/components/LandingComponent.js
+++ src/components/LandingComponent.js
@@ -2,11 +2,17 @@ import React, { Fragment } from "react";
 import "./LandingComponent.css";
 import Media from "react-media";
 
-import { Image, Header } from "semantic-ui-react";
+import { Image, Header, Message } from "semantic-ui-react";
 
 const LandingComponent = () => {
   return (
     <div className="divStyle" style={{ height: "250px" }}>
+      <Message warning>
+        <Message.Header>Changes in Service</Message.Header>
+        <p>
+          We will only be doing <a style={{fontSize: '1.2em', color:'black'}}>take-out</a> and are suspending all dine-in services to aid in slowing the COVID-19 virus outbreak. We apologize for any inconvenience.
+        </p>
+      </Message>
       <Image
         src="https://img1.wsimg.com/isteam/stock/107160/:/rs=h:1026"
         className="image"
@@ -30,7 +36,7 @@ const LandingComponent = () => {
               {matches.small && (
                 <Header
                   size="huge"
-                  style={{ color: "white", position: 'absolute', top: '-40%' }}
+                  style={{ color: "white", position: "absolute", top: "-40%" }}
                 >
                   WELCOME TO THE GOLDEN INN
                 </Header>
--- src/components/desktop/DesktopLandingComponent.js
+++ src/components/desktop/DesktopLandingComponent.js
@@ -1,6 +1,6 @@
 import React from "react";
 import DesktopNavbarComponent from "../Navbar/desktop/DesktopNavbarComponent";
-import { Image, Header } from "semantic-ui-react";
+import { Image, Header, Message } from "semantic-ui-react";
 import LandingComponent from "../LandingComponent";
 
 const LandingComponentStyle = {
@@ -8,30 +8,44 @@ const LandingComponentStyle = {
     textAlign: "center",
     position: "relative",
     textShadow: "0 1px black",
-    height: '500px'
+    height: "500px"
   },
   imageStyle: {
     width: "100%",
-    height: '500px',
+    height: "500px",
     filter: "brightness(55%)",
-    objectFit: 'cover'
+    objectFit: "cover"
   },
   textStyleContainer: {
     position: "relative",
-          left: "50%",
-          transform: "translate(-50%, -125%)",
-          padding: "5%"
+    left: "50%",
+    transform: "translate(-50%, -125%)",
+    padding: "5%"
   },
   textStyle: {
     color: "white",
-    fontSize: '2.3em'
+    fontSize: "2.3em"
   }
 };
 
 const DesktopLandingComponent = props => {
-  const { divStyle, imageStyle, textStyle, textStyleContainer } = LandingComponentStyle;
+  const {
+    divStyle,
+    imageStyle,
+    textStyle,
+    textStyleContainer
+  } = LandingComponentStyle;
   return (
     <div style={divStyle}>
+      <Message warning style={{position:'absolute', zIndex:'1000', width: '100%'}}>
+        <Message.Header>Changes in Service</Message.Header>
+        <p>
+          We will only be doing{" "}
+          <a style={{ fontSize: "1.2em", color: "black" }}>take-out</a> and are
+          suspending all dine-in services to aid in slowing the COVID-19 virus
+          outbreak. We apologize for any inconvenience.
+        </p>
+      </Message>
       <Image
         src="https://img1.wsimg.com/isteam/stock/107160/:/rs=h:1026"
         className="image"
