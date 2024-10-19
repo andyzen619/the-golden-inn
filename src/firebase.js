1d350dd4837495e897a6134b757e9ddfe56db212
andyzen619 <andyzen619@gmail.com>
2024-10-19 10:52:06 -0400

    0 remove API key

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCEjL7ZIpn9SUG8raac9BtH18ZONAD_3hc",
+  apiKey: "NONE",
   projectId: "the-golden-inn-restaurant",
 };
 
